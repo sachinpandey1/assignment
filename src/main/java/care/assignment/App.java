593bb9a9832e4e70ed6d4ec6b2a0c567616391d5
sachinpandey1 <sachinpandey9211@gmail.com>
2021-12-16 11:37:14 +0530

    assignment

---
+++ src/main/java/care/assignment/App.java
@@ -0,0 +1,13 @@
+package care.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
